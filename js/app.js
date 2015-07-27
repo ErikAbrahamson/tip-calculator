1b3b728d7400a98bc1a6a27a5418ff901bd8c0df
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-07-27 10:32:54 -0600

    Add math to function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 console.log('check');
+
+submit.addEventListener('click', function() {
+  returnPercent();
+});
+
+function returnPercent(percentNum) {
+
+  var amount = document.getElementsByTagName('input')[0].value
+  var options = document.getElementsByTagName('select')[0];
+  var percent = percentNum / 100;
+
+  percentNum = options.selectedOptions[0].value;
+
+  return amount * percent;
+}
